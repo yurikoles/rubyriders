19d8b01feec2193b99144fb0a0de2c2c525a4faf
Yuriy Kolesnikov <yurikoles@gmail.com>
2014-09-28 03:57:02 +0300

    #76644416 fix contact spec

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -5,6 +5,6 @@ FactoryGirl.define do
     name 'Tester Girl'
     sequence(:email_or_phone) {|n| "test#{n}@example.com or +#{Random.rand(10000000000)}"}
     question_or_feedback 'My question or feedback'
-    sequence(:ip_address) {"12.34.56.78#{n}"}
+    sequence(:ip_address) {|n| "12.34.56.78#{n}"}
   end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,25 +3,31 @@ require 'rails_helper'
 describe Contact do
   let(:contact) { FactoryGirl.create(:contact) }
 
+  same_ip = '11.22.33.444'
+
   context "valid attributes" do
     it "1 ip should be valid" do
       expect(contact.valid?).to eq(true)
     end
+    
+    it "4 same ip should be valid" do
+      FactoryGirl.create_list(:contact, 3, :ip_address => same_ip)
+      contact4 = FactoryGirl.build(:contact, :ip_address => same_ip)
+      expect(contact4.valid?).to eq(true)
+    end
 
-    it "4 ip should be valid" do
-      (1..3).each do
-        FactoryGirl.create(:contact, :ip_address => '11.22.33.444')
-      end
-      expect(FactoryGirl.build(:contact, :ip_address => '11.22.33.444').valid?).to eq(true)
+    it "5 different ip should be valid" do
+      FactoryGirl.create_list(:contact, 4)
+      contact5 = FactoryGirl.build(:contact)
+      expect(contact5.valid?).to eq(true)
     end
   end
 
   context "invalid attributes" do
-    it  "5 ip should not be valid" do
-      (1..4).each do
-        FactoryGirl.create(:contact, :ip_address => '11.22.33.444')
-      end
-      expect(FactoryGirl.build(:contact, :ip_address => '11.22.33.444').valid?).to eq(false)
+    it "5 same ip should be invalid" do
+      FactoryGirl.create_list(:contact, 4, :ip_address => same_ip)
+      contact5 = FactoryGirl.build(:contact, :ip_address => same_ip)
+      expect(contact5.valid?).to eq(false)
     end
   end
 end
