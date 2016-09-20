cb743846c88f5c64106640f535411aaeac4d89be
Yurii Kolesnykov <yurikoles@gmail.com>
2016-09-20 20:05:15 +0300

    add client create

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,9 @@
 class ClientsController < ApplicationController
   def index
+    Client.all
   end
   @cloverpops = {black:'cloverpop-color.png', white:'cloverpop.png'}
+  def create
+    client = Client.new(name:'Some Client', short_description: 'Client description', description: 'Full descritption')
+  end
 end
--- test/controllers/why_us_controller_test.rb
+++ test/controllers/why_us_controller_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class WhyUsControllerTest < ActionController::TestCase
+  def setup
+    @controller = WhyUsController.new
+  end
   test "should get index" do
     get :index
     assert_response :success
